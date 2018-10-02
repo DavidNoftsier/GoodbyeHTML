c5e3abd72ebc93f2d9ecf121ed2d85c9a030f566
David Noftsier <dnoftsier@LeakyCauldron.local>
2018-10-02 17:49:55 -0400

    Adding logic for creating stored list, add ability to block additional elements

---
+++ blockElements.js
@@ -0,0 +1,17 @@
+      
+      document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('block-element').addEventListener('click', function(){
+
+          var element = document.getElementById('element').value;
+          addElement(element);
+        });
+      });
+
+      function addAlement (element){
+          chrome.storage.sync.get(['elementsToDestroy'], function (result) {
+            // TODO Check that there are no duplicates
+            var updatedElementsToDestroy = result.elementsToDestroy.push(element);
+            // TODO Try and find a better way to do this than replacing the entire object each time
+            chrome.storage.sync.set({elementsToDestroy: updatedElementsToDestroy}, function (){});
+          });
+      }
