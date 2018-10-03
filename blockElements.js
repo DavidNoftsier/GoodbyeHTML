eef95f38d0b9ce5a13b2b1e1265dbf4a8bde12d9
David Noftsier <dnoftsier@LeakyCauldron.local>
2018-10-03 09:38:03 -0400

    Adding Jasmine to project

--- blockElements.js
+++ blockElements.js
@@ -10,7 +10,8 @@
       var addElement = function (element){
           chrome.storage.sync.get(['elementsToDestroy'], function (result) {
             // TODO Check that there are no duplicates
-            var updatedElementsToDestroy = result.elementsToDestroy.push(element);
+            result.elementsToDestroy.push(element);
+            var updatedElementsToDestroy = result.elementsToDestroy;
             // TODO Try and find a better way than replacing the entire object each time
             chrome.storage.sync.set({elementsToDestroy: updatedElementsToDestroy}, function (){});
           });
