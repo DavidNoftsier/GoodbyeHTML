8a415432ac3f13c23b2761d643f03803b86626c5
David Noftsier <dnoftsier@gmail.com>
2018-11-01 12:36:53 -0400

    Adding logic for finding element XPath (#1)

--- block-elements.js
+++ block-elements.js
@@ -1,7 +1,7 @@
+
 chrome.storage.sync.get(['elementsToBlockEverywhere'], function (result) {
 	for(let i = 0; i< result.elementsToBlockEverywhere.length; i++){
 		let elements = document.querySelectorAll(result.elementsToBlockEverywhere[i]);
-		debugger;
 		for(let i = 0; i < elements.length; i++)
 			elements[i].parentNode.removeChild(elements[i]);
 	}
@@ -9,7 +9,6 @@ chrome.storage.sync.get(['elementsToBlockEverywhere'], function (result) {
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
-	debugger;
 	switch(request.action){
 		case 'destroyElements': 
 			destroyElements(request.data);
@@ -18,10 +17,59 @@ chrome.runtime.onMessage.addListener(
 });
 
 const destroyElements = (elementSelector) => {
-  debugger;
   let elements = document.querySelectorAll(elementSelector);
   for(let i = 0; i < elements.length; i++)
     elements[i].parentNode.removeChild(elements[0]);
 }
 
+handleMouseover = (event) => {
+	debugger;
+	let xpath = getElementXPath(event.target);
+}
+document.addEventListener('mouseover', handleMouseover);
 
+// getPathTo
+// https://stackoverflow.com/questions/2631820/how-do-i-ensure-saved-click-coordinates-can-be-reloaed-to-the-same-place-even-i/2631931#2631931
+// Author: https://stackoverflow.com/users/18936/bobince
+
+// export function prepGetPath = (event) => {
+//     if (event===undefined) event= window.event;                     // IE hack
+//     var target= 'target' in event? event.target : event.srcElement; // another IE hack
+
+//     // var root= document.compatMode==='CSS1Compat'? document.documentElement : document.body;
+//     // var mxy= [event.clientX+root.scrollLeft, event.clientY+root.scrollTop];
+
+//     var path= getPathTo(target);
+//     // var txy= getPageXY(target);
+//     // alert('Clicked element '+path+' offset '+(mxy[0]-txy[0])+', '+(mxy[1]-txy[1]));
+//     return path;
+// }
+
+getElementXPath = (element) => {
+    if (element.id!=='')
+        return 'id("'+element.id+'")';
+    if (element===document.body)
+        return element.tagName;
+
+    var ix= 0;
+    var siblings= element.parentNode.childNodes;
+    for (var i= 0; i<siblings.length; i++) {
+        var sibling= siblings[i];
+        if (sibling===element)
+            return getElementXPath(element.parentNode)+'/'+element.tagName+'['+(ix+1)+']';
+        if (sibling.nodeType===1 && sibling.tagName===element.tagName)
+            ix++;
+    }
+}
+
+//export default getElementXPath;
+
+// getPageXY = (element) => {
+//     var x= 0, y= 0;
+//     while (element) {
+//         x+= element.offsetLeft;
+//         y+= element.offsetTop;
+//         element= element.offsetParent;
+//     }
+//     return [x, y];
+// }
